4bcc1e8d3e823f604309c03684cf41e9c506fcab
gyab <jsbe.antoine@gmail.com>
2020-05-22 22:32:31 +0200

    small fixes PEP8

--- crawling/crawling/spiders/test.py
+++ crawling/crawling/spiders/test.py
@@ -22,8 +22,12 @@ class ViePubliqueDiscours(scrapy.Spider):
         discours = SpeechItem()
         discours['title'] = response.css('h1::text').get().strip()
         discours['date'] = response.css('time::attr(datetime)').get()
-        discours['text'] = response.css('.field--name-field-texte-integral p').get()
+        discours['text'] = response \
+            .css('.field--name-field-texte-integral p') \
+            .get()
         discours['tags'] = response.css("div.tagsBox a::text").getall()
-        discours['topics'] = response.css("div.thematicBox a::text").getall()
-        discours['speakers'] = response.css("ul.line-intervenant a::text").getall()
+        discours['topics'] = response.css("div.thematicBox a::text") \
+            .getall()
+        discours['speakers'] = response \
+            .css("ul.line-intervenant a::text").getall()
         return dict(discours)
