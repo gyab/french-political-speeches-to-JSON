2b78b803d319aed8f146b8d0d9c193fa8b3d91cf
gyab <jsbe.antoine@gmail.com>
2020-09-20 08:49:37 +0200

    added trim to topics

--- crawling/crawling/spiders/speeches.py
+++ crawling/crawling/spiders/speeches.py
@@ -11,9 +11,8 @@ class ViePubliqueDiscours(scrapy.Spider):
         speech_page_links = response.css('.teaserSimple--title a')
         yield from response.follow_all(speech_page_links, self.parse_speech)
 
-        pagination_links = response.css('li.pager__item a')
-        print(pagination_links)
-        yield from response.follow_all(pagination_links, self.parse)
+        # pagination_links = response.css('li.pager__item a')
+        # yield from response.follow_all(pagination_links, self.parse)
 
     def parse_speech(self, response):
         discours = SpeechItem()
@@ -23,7 +22,7 @@ class ViePubliqueDiscours(scrapy.Spider):
             .css('.field--name-field-texte-integral p') \
             .get()
         discours['tags'] = response.css("div.tagsBox a::text").getall()
-        discours['topics'] = response.css("div.thematicBox a::text").getall()
+        discours['topics'] = [x.strip() for x in response.css("div.thematicBox a::text").getall()]
         discours['speakers'] = response \
             .css("ul.line-intervenant a::text").getall()
         return dict(discours)
