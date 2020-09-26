aab0c23d81c2e5fa5b97bac40331315f6981701b
gyab <jsbe.antoine@gmail.com>
2020-09-26 18:44:41 +0200

    add start and end date for scraping

--- crawling/spiders/statements.py
+++ crawling/spiders/statements.py
@@ -1,25 +1,28 @@
-import scrapy
 from datetime import datetime
+import scrapy
 
 
 class ViePubliqueDiscours(scrapy.Spider):
-    name = 'speeches'
-
+    name = 'statements'
     start_urls = ['https://www.vie-publique.fr/discours']
 
     def parse(self, response):
+        start_date = datetime.strptime(self.start_date, '%Y-%m-%d').date()
+        end_date = datetime.strptime(self.end_date, '%Y-%m-%d').date()
         statements = {
             'links': response.css('.teaserSimple--title a'),
             'dates': response.css('time::attr(datetime)').getall()
         }
         statements_links = []
         for i, statement_date in enumerate(statements['dates']):
-            if datetime.strptime(statement_date, "%Y-%m-%dT%H:%M:%SZ").date() == datetime.now().date():
+            statement_date = datetime.strptime(statement_date, "%Y-%m-%dT%H:%M:%SZ").date()
+            if statement_date <= start_date and statement_date >= end_date:
                 statements_links.append(statements['links'][i])
         if statements_links:
             yield from response.follow_all(statements_links, self.parse_speech)
 
-        if datetime.strptime(response.css('time::attr(datetime)').getall()[-1], "%Y-%m-%dT%H:%M:%SZ").date() == datetime.now().date():
+        page_last_statement_date = datetime.strptime(response.css('time::attr(datetime)').getall()[-1], "%Y-%m-%dT%H:%M:%SZ").date()
+        if page_last_statement_date >= end_date:
             pagination_links = response.css('li.pager__item a')
             yield from response.follow_all(pagination_links, self.parse)
 
